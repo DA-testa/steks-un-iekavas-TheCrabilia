a4902d62b4e8a41e7224c22703a320c69fde22c4
Ilja Spirts <69972734+TheCrabilia@users.noreply.github.com>
2023-02-13 11:14:15 +0000

    Add mode selector

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
-from collections import namedtuple
-
-Bracket = namedtuple("Bracket", ["char", "position"])
+import os
 
 
 def are_matching(left: str, right: str) -> bool:
     return (left + right) in ["()", "[]", "{}"]
 
 
-def find_mismatch(text: str) -> int:
+def find_mismatch(text: str) -> str:
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(next)
         if next in ")]}":
             if not opening_brackets_stack:
-                return i + 1
-            prev = opening_brackets_stack[-1]
-            if not are_matching(prev, next):
-                return i + 1
-            opening_brackets_stack.pop()
-
+                return str(i + 1)
+            if are_matching(opening_brackets_stack[-1], next):
+                opening_brackets_stack.pop()
+            else:
+                return str(i + 1)
     if opening_brackets_stack:
-        return len(opening_brackets_stack)
-
-    return 0
+        return str(i + 1)
+    return "Success"
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    print(mismatch if mismatch != 0 else "Success")
+    mode = input("Choose mode (I - manual input, F - read from file): ").upper()
+    match mode:
+        case "I":
+            text = input()
+            print(find_mismatch(text))
+        case "F":
+            for i in range(len(os.listdir("test")) // 2):
+                print(f"Running test {i}... ", end="")
+                with open(f"test/{i}", "r") as fi, open(
+                    f"test/{i}.a", "r"
+                ) as fa:
+                    input_data = fi.readline()
+                    answer_data = fa.readline().strip()
+                    mismatch = find_mismatch(input_data)
+                    if mismatch != answer_data:
+                        print(
+                            f"Test failed, want {answer_data}, got {mismatch}"
+                        )
+                    else:
+                        print("Success")
+        case _:
+            print("Invalid mode")
+            os.exit()
 
 
 if __name__ == "__main__":
